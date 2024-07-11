65341fa5370ba8d4dd76ec8eb2cae0f7848dbf90
Ankit Kumar Sharma <ankit.sharma@qontenttai.com>
2024-07-11 23:19:00 +0530

    adding changes

---
+++ src/components/specific/Notification.jsx
@@ -0,0 +1,12 @@
+import { Dialog } from '@mui/material'
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <Dialog>
+
+        </Dialog>
+    )
+}
+
+export default Notification
